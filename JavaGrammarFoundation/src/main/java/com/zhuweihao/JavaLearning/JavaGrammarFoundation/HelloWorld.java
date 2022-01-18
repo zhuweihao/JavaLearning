462adfa31fc77e86f6f2e5d5d0b7ae509f4cf2c8
zhuweihao <2548237631@qq.com>
2022-01-18 10:53:13 +0800

    初始化仓库

---
+++ JavaGrammarFoundation/src/main/java/com/zhuweihao/JavaLearning/JavaGrammarFoundation/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zhuweihao.JavaLearning.JavaGrammarFoundation;
+
+/**
+ * @Author zhuweihao
+ * @Date 2022/1/18 10:44
+ * @Description com.zhuweihao.JavaLearning.JavaGrammarFoundation
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
